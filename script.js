b3af0aecbb88dc26e7d650d32b33497a13d2127d
NoSword <alexjonesc@gmail.com>
2021-06-18 14:22:07 -0700

    lesson complete

--- script.js
+++ script.js
@@ -4,9 +4,23 @@ const quoteText = document.getElementById("quote");
 const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+let apiErrorRetries = 0;
+
+function showLoadingSpinner() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
 
 // Show New Quote
 function newQuote() {
+  showLoadingSpinner();
+
   // Pick random quote
   const idx = Math.floor(Math.random() * apiQuotes.length);
   const quote = apiQuotes[idx];
@@ -15,25 +29,43 @@ function newQuote() {
 
   // check quote length
   if (quote.text.length > 120) {
-    quoteText.classList.add('long-quote');
+    quoteText.classList.add("long-quote");
   } else {
-    quoteText.classList.remove('long-quote');
+    quoteText.classList.remove("long-quote");
   }
 
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get Quotes From API
 async function fetchQuotes() {
+  showLoadingSpinner();
   const apiUrl = "https://type.fit/api/quotes";
   try {
     const response = await fetch(apiUrl);
     apiQuotes = await response.json();
     newQuote();
   } catch (error) {
-    console.log(error);
+    if (apiErrorRetries === 3) {
+      console.log(error);
+    } else {
+      apiErrorRetries++;
+      fetchQuotes();
+    }
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.innerText} - ${authorText.innerText}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
 
 // On Load
 document.addEventListener("DOMContentLoaded", function () {
